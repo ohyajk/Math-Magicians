32b378db26bb994d957a863e9e04bba41e67db25
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-08 19:29:42 +0530

    Add CRA with Linters & TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="bg-blue-900 text-white">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold">
+          Hail Tailwind CSS 🙌
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
