67c297976f180379fac45cbea36e20f3b6f71ac5
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-09 18:46:32 +0530

    Add Class based Component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React, { Component } from 'react';
+
 import Calculator from './components/calculator';
-
-function App() {
-  return (
-    <Calculator />
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
-
-export default App;
