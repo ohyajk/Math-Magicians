7a7fc7330e5de3b21d74019b3fbe05e533854b80
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 12:20:09 +0530

    Add HomePage

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,27 @@
+const HomePage = () => (
+  <div>
+    <h2 className="text-4xl py-16 px-4">Welcome To Our Page</h2>
+    <p className="py-8 px-4">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Eaque temporibus voluptates perferendis fugiat corporis magni?
+      perspiciatis perferendis vero nam quibusdam ea, iure optio veritatis reiciendis
+      Reprehenderit, accusamus neque? Repellat exercitationem excepturi illo inventore
+      Lorem ipsum dolor sit amet, consectetur adipisicing eliure, illum consequatur esse
+      Omnis velit vel quam architecto voluptas, cum nostrum neqminus facere
+      Debitis porro praesentium placeat laudantium aperisapiente id magnam perspiciatis i
+      Repellat rem consequatur ducimus expedita magnam ex! Eanam dignissimos? Dolorem deser
+    </p>
+    <p className="py-8 px-4">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Eaque temporibus voluptates perferendis fugiat corporis magni?
+      perspiciatis perferendis vero nam quibusdam ea, iure optio veritatis reiciendis
+      Reprehenderit, accusamus neque? Repellat exercitationem excepturi illo inventore
+      Lorem ipsum dolor sit amet, consectetur adipisicing eliure, illum consequatur esse
+      Omnis velit vel quam architecto voluptas, cum nostrum neqminus facere
+      Debitis porro praesentium placeat laudantium aperisapiente id magnam perspiciatis i
+      Repellat rem consequatur ducimus expedita magnam ex! Eanam dignissimos? Dolorem deser
+    </p>
+  </div>
+);
+
+export default HomePage;
