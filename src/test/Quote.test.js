058ed1222f4c45bc7c1be17935a7fe9caae954ac
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 16:02:36 +0530

    Add tests

---
+++ src/test/Quote.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import QuotePage from '../pages/QuotePage';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<QuotePage />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
