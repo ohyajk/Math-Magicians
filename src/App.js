09a424ea981f560accb42d4e81d88843c2f5a812
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 12:29:01 +0530

    Add Routes of pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './Pages/HomePage';
+import QuotePage from './Pages/QuotePage';
+import CalculatorPage from './Pages/CalculatorPage';
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/calculator" element={<CalculatorPage />} />
+      <Route path="/quote" element={<QuotePage />} />
     </Routes>
   </BrowserRouter>
 );
