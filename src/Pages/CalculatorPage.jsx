a7a1b5cd5c07cc4c3f96aeb7d5348f2e83d6174b
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 12:55:11 +0530

    Update Calculator Page

---
+++ src/Pages/CalculatorPage.jsx
@@ -0,0 +1,10 @@
+import Calculator from '../components/calculator';
+
+const CalculatorPage = () => (
+  <div>
+    <h2>Lets Do Some Math...</h2>
+    <Calculator />
+  </div>
+);
+
+export default CalculatorPage;
