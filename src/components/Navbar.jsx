b3701b12e74be1d0c396c4349568d03ebb9232b0
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 12:51:41 +0530

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,14 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>
-      <nav className="text-white text-center flex gap-10">
-        <h1>Logo</h1>
-        <Link to="/">HOME</Link>
-        <Link to="/about">ABOUT</Link>
+    <div className="px-4 py-2">
+      <nav className="text-white text-center flex items-center justify-between">
+        <h1 className="text-amber-500 text-4xl">Math Magicians</h1>
+        <div className="text-lg flex gap-10">
+          <Link className="hover:text-amber-500" to="/">HOME</Link>
+          <Link className="hover:text-amber-500" to="/calculator">CALCULATOR</Link>
+          <Link className="hover:text-amber-500" to="/quote">QUOTE</Link>
+        </div>
       </nav>
     </div>
   );
