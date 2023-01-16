308c94bfa179fae8fdcec6bdbc6acd304cd1908f
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-16 12:20:20 +0530

    Add QuotePage

---
+++ src/Pages/QuotePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuotePage = () => (
+  <div>
+    <blockquote>
+      “Creativity is the greatest rebellion in existence.”
+    </blockquote>
+  </div>
+);
+
+export default QuotePage;
